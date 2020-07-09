639fe2acd19b804909892c3c139b6260f2a172a3
Priyank <priyank.agg6@gmail.com>
2020-07-09 05:39:21 +0530

    update for heroku

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoicHJpeWFuazk3IiwiYSI6ImNrYzJhdTdrZjF2NHEycm5haTZiMzdvYzcifQ.AEZGsFqg7S7SD0YTNhgX_A&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
